a0fcfb09d3a22cf8d77b0168346ae76be31147f8
A. Riddell <riddella@indiana.edu>
2020-08-17 14:54:47 -0400

    docs: Update docs for 2020
    - Update version in What's New.
    - Remove mention of google groups mailing list.
    - Use autoapi (instead of autodoc) for compatibility with readthedocs

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -9,10 +9,12 @@ sys.path.insert(0, os.path.abspath('../..'))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
-    'sphinx.ext.autodoc',
     'sphinx_rtd_theme',
     'numpydoc',
+    'autoapi.extension',
 ]
+autoapi_dirs = [os.path.join("..", "..", "lda")]
+autoapi_ignore = ["*tests*"]
 
 # autodoc generation is a bit aggressive and a nuisance when doing heavy
 # text edit cycles.
